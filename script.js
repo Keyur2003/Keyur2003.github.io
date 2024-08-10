f4db468adc1f4965a704fb44f1ecaf9c9056f085
Keyur Sanghani <77490111+Keyur2003@users.noreply.github.com>
2024-08-11 01:59:24 +0530

    feature(Portfolio) Worked on the portfolio module

--- script.js
+++ script.js
@@ -75,14 +75,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer",],
+        strings: ["Web Developer","Back-end Developer","Laravel Developer","Vue Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer"],
+        strings: ["Web Developer","Back-end Developer","Laravel Developer","Vue Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
